47dd05321ea0f3375c12772cc9ef72139a7ce35d
Alan Gazielly <alan@gazielly.com>
2019-08-14 17:11:46 +0200

    Rake all green. Completed before refacto.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,8 @@
 class Restaurant < ApplicationRecord
+  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
-  validates :category, presence: true, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
+  validates :category, presence: true, inclusion: { in: CATEGORY }
   has_many :reviews, dependent: :destroy
 end
